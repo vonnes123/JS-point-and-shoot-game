d53f96c748ddcc651fd346498a0e7e2efe7a1428
Vasil <vasil.todorov@student.fontys.nl>
2022-10-19 22:35:40 +0200

    set up movement for ravens

--- script.js
+++ script.js
@@ -2,3 +2,44 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+let timeToNextRaven = 0;
+let ravenInterval = 500;
+let lastTime = 0;
+
+let ravens = [];
+class Raven {
+    constructor(){
+        this.width = 100;
+        this.height = 50;
+        this.x = canvas.width;
+        this.y = Math.random() * (canvas.height - this.height);
+        this.directionX = Math.random() * 5 + 3;
+        this.directionY = Math.random() * 5 - 2.5;
+        this.markedForDeletion = false;
+    }
+    update(){
+        this.x -= this.directionX;
+        if (this.x < 0 - this.width) this.markedForDeletion = true
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+}
+
+const raven = new Raven();
+
+function animate(timestamp){
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    let deltatime = timestamp - lastTime;
+    lastTime = timestamp;
+    timeToNextRaven += deltatime;
+    if (timeToNextRaven > ravenInterval){
+        ravens.push(new Raven());
+        timeToNextRaven = 0;
+    }
+    [...ravens].forEach(object => object.update());
+    [...ravens].forEach(object => object.draw());
+    requestAnimationFrame(animate);
+}
+animate(0);
