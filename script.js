6af0073f3a892df94bef8e2affe705b44601b298
Vasil <vasil.todorov@student.fontys.nl>
2022-10-21 18:35:54 +0200

    add particles

--- script.js
+++ script.js
@@ -36,6 +36,7 @@ class Raven {
         this.flapInterval = Math.random() * 50 + 50;
         this.randomColors = [Math.floor(Math.random() * 255), Math.floor(Math.random() * 255), Math.floor(Math.random() * 255)];
         this.color = 'rgb(' + this.randomColors[0] + ',' + this.randomColors[1] + ',' + this.randomColors[2] + ')';
+        this.hasTrail = Math.random() > 0.5;
     }
     update(deltatime){
         if (this.y < 0 || this.y > canvas.height - this.height){
@@ -49,6 +50,13 @@ class Raven {
             if (this.frame > this.maxFrame) this.frame = 0;
             else this.frame++;
             this.timeSinceFlap = 0;
+            if (this.hasTrail) {
+                for(let i = 0; i < 5; i++){
+                    particles.push(
+                        new Particle(this.x, this.y, this.width, this.color)
+                    );
+                }
+            }
         }
         if(this.x < 0 - this.width) gameOver = true;
     }
@@ -86,6 +94,34 @@ class Explosion {
     }
     draw(){
         ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y - this.size/4, this.size, this.size);
+    }
+}
+
+let particles = [];
+class Particle {
+    constructor(x, y, size, color){
+        this.size = size;
+        this.x = x + this.size/2 + Math.random() * 50 - 25;
+        this.y = y + this.size/3 + Math.random() * 50 - 25;
+        this.radius = Math.random() * this.size/10;
+        this.maxRadius = Math.random() * 20 + 35;
+        this.markedForDeletion = false;
+        this.speedX = Math.random() * 1 + 0.5;
+        this.color = color;
+    }
+    update(){
+        this.x += this.speedX;
+        this.radius += 0.3;
+        if (this.radius > this.maxRadius - 5) this.markedForDeletion = true;
+    }
+    draw(){
+        ctx.save();
+        ctx.globalAlpha = 1 - this.radius/this.maxRadius;
+        ctx.beginPath();
+        ctx.fillStyle = this.color;
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.restore();
     }
 }
 
@@ -132,10 +168,11 @@ function animate(timestamp){
         })
     }
     drawScore();
-    [...ravens, ...explosion].forEach(object => object.update(deltatime));
-    [...ravens, ...explosion].forEach(object => object.draw());
+    [ ...particles, ...ravens, ...explosion].forEach(object => object.update(deltatime));
+    [...particles, ...ravens, ...explosion].forEach(object => object.draw());
     ravens = ravens.filter(object => !object.markedForDeletion);
     explosion = explosion.filter(object => !object.markedForDeletion);
+    particles = particles.filter(object => !object.markedForDeletion);
     if (!gameOver) requestAnimationFrame(animate);
     else drawGameOver();
 }   
